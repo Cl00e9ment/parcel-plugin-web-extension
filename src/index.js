65dc6b113cbbf41a7d0fe45c86b6d35a58fe4da5
Clément Saccoccio <clem.saccoccio@protonmail.com>
2020-01-26 22:32:54 +0100

    update entry files atime and mtime to avoid caching

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const glob = require('fast-glob')
+const fs = require('fs');
 
 function WebExtensionPlugin(bundler) {
     bundler.addAssetType('json', require.resolve('./ManifestAsset'))
@@ -9,6 +10,16 @@ function WebExtensionPlugin(bundler) {
         path.resolve(options.rootDir, '_locales/**/messages.json')
     )
     bundler.entryFiles.push(...locales)
+
+    // update entry files atime and mtime to avoid caching
+    const now = Date.now();
+    for (const file of bundler.entryFiles) {
+        try {
+            fs.utimesSync(file, now, now);
+        } catch (err) {
+            fs.closeSync(fs.openSync(file, 'w'));
+        }
+    }
 }
 
 module.exports = WebExtensionPlugin
